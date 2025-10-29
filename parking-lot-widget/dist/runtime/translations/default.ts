d0a99fdce829b9e3c05a2aef1994e72967a8f608
Finn <finn.morris00@gmail.com>
2025-10-29 11:07:37 +0800

    Add files via upload

---
+++ parking-lot-widget/dist/runtime/translations/default.ts
@@ -0,0 +1,11 @@
+export default {
+  _widgetLabel: 'Parking Lot Widget',
+  parkingLotStatus: 'Parking Lot Status',
+  noDataSource: 'No data source configured',
+  loadingData: 'Loading parking lot data...',
+  errorLoadingData: 'Error loading data source',
+  noDataAvailable: 'No parking lot data available',
+  configureDataSource: 'Please configure a data source in the widget settings'
+}
+
+
---
+++ parking-lot-widget/dist/setting/translations/default.ts
@@ -0,0 +1,18 @@
+export default {
+  parkingLotField: 'Parking Lot Field',
+  utilizationField: 'Utilization Field',
+  showUtilizationText: 'Show Utilization Text',
+  allowNegatives: 'Allow Negative Values',
+  fullThreshold: 'Full % Threshold',
+  dataFormatExpected: 'Data Format Expected',
+  dataFormatDescription: 'Your data source should contain fields for parking lot names and utilization percentages (0-100). The widget will display color-coded dots based on utilization levels.',
+  colorRanges: 'Color Ranges',
+  addColorRange: 'Add Color Range',
+  removeColorRange: 'Remove',
+  minValue: 'Min Value',
+  maxValue: 'Max Value',
+  color: 'Color',
+  label: 'Label',
+  rangeOverlapWarning: 'Warning: Ranges should not overlap',
+  invalidRangeWarning: 'Warning: Min value must be less than max value'
+}
